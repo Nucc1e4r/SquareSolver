5b5d20d784ac4477c9b6f34f116503b12981de3d
Nucc1e4r <vladzarembo73@gmail.com>
2023-08-22 14:58:34 +0300

    Добавил ассерты, реализовал кол-во корней через enum, вынес ввод/вывод в отдельные функции

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
-// Assigning a code to the situation with infinite amount of solutions:
-    const int EQ_INF_SOL = -1;
+// Creating codes for all numbers of solutions:
+    enum sols {ZERO, ONE, TWO, INF};
 
 // Declaring functions:
     bool isZero (double x);
 
-    int solveSquare (const double a, const double b, const double c, double* x1, double* x2);
-    int solveLinear (const double a, const double b, double* x);
+    void inputSquareData(double* a, double* b, double* c);
+    void outputSquareSolution(sols nRoots, double* x1, double* x2);
+
+    sols solveSquare (const double a, const double b, const double c, double* x1, double* x2);
+    sols solveLinear (const double a, const double b, double* x);
 
 //----------------------------------------------------------------------------------
 
@@ -21,18 +25,11 @@ int main () {
 
     double a = 0, b = 0, c = 0;
 
-    printf("Input a:\n");
-    scanf("%lg", &a);
-
-    printf("Input b:\n");
-    scanf("%lg", &b);
-
-    printf("Input c:\n");
-    scanf("%lg", &c);
+    inputSquareData(&a, &b, &c);
 
 
     double x1 = 0, x2 = 0;
-    int nRoots = 0;
+    enum sols nRoots = ZERO;
 
     nRoots = solveSquare(a, b, c, &x1, &x2);
 
@@ -40,28 +37,17 @@ int main () {
         if (isZero(x1)) x1 = 0;
         if (isZero(x2)) x2 = 0;
 
-    switch (nRoots) {
-        case 2:
-            printf("x1 = %lg; x2 = %lg", x1, x2);
-            break;
-        case 1:
-            printf("x = %lg", x1);
-            break;
-        case 0:
-            printf("No real solutions");
-            break;
-        case EQ_INF_SOL:
-            printf("x is any");
-            break;
-        default:
-            printf("!NUMBER OF ROOTS INTERPRETATION ERROR!");
-    }
+    outputSquareSolution(nRoots, &x1, &x2);
+
     return 0;
 }
 
 //----------------------------------------------------------------------------------
 
 bool isZero (double x) {
+
+    assert(isfinite(x));
+
     double eps = 0.00000001;
     if (fabs(x) < eps) {
         return true;
@@ -69,7 +55,52 @@ bool isZero (double x) {
     return false;
 }
 
-int solveSquare (const double a, const double b, const double c, double* x1, double* x2) {
+void inputSquareData(double* a, double* b, double* c) {
+
+    assert(a != b);
+    assert(b != c);
+    assert(a != c);
+
+    printf("Input a:\n");
+    scanf("%lg", a);
+
+    printf("Input b:\n");
+    scanf("%lg", b);
+
+    printf("Input c:\n");
+    scanf("%lg", c);
+}
+
+void outputSquareSolution(sols nRoots, double* x1, double* x2) {
+
+    assert(x1 != x2);
+
+    switch (nRoots) {
+        case TWO:
+            printf("x1 = %lg; x2 = %lg", *x1, *x2);
+            break;
+        case ONE:
+            printf("x = %lg", *x1);
+            break;
+        case ZERO:
+            printf("No real solutions");
+            break;
+        case INF:
+            printf("x is any");
+            break;
+        default:
+            printf("!NUMBER OF ROOTS INTERPRETATION ERROR!");
+    }
+}
+
+sols solveSquare (const double a, const double b, const double c, double* x1, double* x2) {
+
+    assert(isfinite(a));
+    assert(isfinite(b));
+    assert(isfinite(c));
+
+    assert(x1 != x2);
+
     if (isZero(a)) {
         return solveLinear(b, c, x1);
     }
@@ -79,28 +110,32 @@ int solveSquare (const double a, const double b, const double c, double* x1, dou
         if (D > 0) {
             *x1 = (-b + sqrtD) / (2*a);
             *x2 = (-b - sqrtD) / (2*a);
-            return 2;
+            return TWO;
         }
         else if (isZero(D)) {
             *x1 = -b / (2*a);
-            return 1;
+            return ONE;
         }
         else {
-            return 0;
+            return ZERO;
         }
     }
 }
 
 
-int solveLinear (const double a, const double b, double* x) {
+sols solveLinear (const double a, const double b, double* x) {
+
+    assert(isfinite(a));
+    assert(isfinite(b));
+
     if (isZero(a)) {
         if (isZero(b)) {
-            return EQ_INF_SOL;
+            return INF;
         }
-        return 0;
+        return ZERO;
     }
     else /* if (a != 0) */ {
         *x = -b / a;
-        return 1;
+        return ONE;
     }
 }
